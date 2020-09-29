7e7d7c5fe5dd67ba7a7b9fbed8e02c89826052eb
Mark Gallagher <mark@fts.scot>
2020-09-29 22:05:31 +0100

    implement go.sh

--- go.sh
+++ go.sh
@@ -1,24 +1,50 @@
-# TODO: Helper script - automate the mundane !
+#!/bin/bash
 
-# This script should do several things.
+# This script installs the firmware and topologies in /lib/firmware/intel.
+# Optionally if ROOT is set as an environment variable by the caller it will be
+# prepended to the installation path.
 #
-# 1) Install the firmware and topologies in the correct target directories
-#    depending on PREFIX and vendor. PREFIX should be set as cmd line argument
-#    otherwise default to /lib/firmware. "install" rule.
-#
-# 2) As 1, but for the ldc files. This is the "install_debug" rule.
-#
-# 3) Take a SOF tag as cmd line argument, checkout tag locally, build topologies
-#    and copy them here under tag name directory. "update-tplg" rule.
-#
-# 4) As 3, but for firmware and sign with public key. Build with XCC where
-#    possible or download binaries (signed with any key) from external resource.
-#    "update-firmware" rule.
-#
-# 5) Publish all new topologies and firmwares by commiting and pushing this
-#    local copy of repo to upstream master. This should check for any missing
-#    files (e.g. signed public releases should match signed intel releases).
-#    "publish" rule. 
+# The script will attempt to use git to work out the version automatically, if
+# that is not possible on your environment specify SOF_VERSION as an environment
+# variable when calling.
 
-echo "Nothing implemented"
+test -n "${ROOT}" ||  \
+    ROOT=
+test -n "${INTEL_PATH}" || \
+    INTEL_PATH=lib/firmware/intel
+test -n "${SOF_VERSION}" || \
+    SOF_VERSION=$(git symbolic-ref HEAD 2>/dev/null | cut -d"/" -f 3| cut -d"-" -f 2)
+test -n "${SOF_VERSION}" || \
+    { echo "Can't work out SOF_VERSION using git, please specify SOF_VERSION as environment variable"; exit 1; }
 
+test -d ${INTEL_PATH}/sof-tplg-${SOF_VERSION} || \
+    { echo "Can't find version ${SOF_VERSION} - are you missing leading v?"; exit 2; }
+
+echo "Installing Intel firmware and topology $SOF_VERSION to $INTEL_PATH"
+
+# wipe previous releases
+rm -rf ${ROOT}/${INTEL_PATH}/sof/*
+rm -rf ${ROOT}/${INTEL_PATH}/sof-tplg-*
+rm -rf ${ROOT}/${INTEL_PATH}/sof-tplg
+
+# copy to destination
+cd lib/firmware
+cp -rf intel ${ROOT}/lib/firmware
+
+# add symlinks
+cd ${ROOT}/${INTEL_PATH}/sof
+
+ln -s ${SOF_VERSION}/sof-bdw-${SOF_VERSION}.ri sof-bdw.ri
+ln -s ${SOF_VERSION}/sof-byt-${SOF_VERSION}.ri sof-byt.ri
+ln -s ${SOF_VERSION}/sof-cht-${SOF_VERSION}.ri sof-cht.ri
+ln -s ${SOF_VERSION}/intel-signed/sof-apl-${SOF_VERSION}.ri sof-apl.ri
+ln -s ${SOF_VERSION}/intel-signed/sof-apl-${SOF_VERSION}.ri sof-glk.ri
+ln -s ${SOF_VERSION}/intel-signed/sof-cnl-${SOF_VERSION}.ri sof-cfl.ri
+ln -s ${SOF_VERSION}/intel-signed/sof-cnl-${SOF_VERSION}.ri sof-cnl.ri
+ln -s ${SOF_VERSION}/intel-signed/sof-cnl-${SOF_VERSION}.ri sof-cml.ri
+ln -s ${SOF_VERSION}/intel-signed/sof-icl-${SOF_VERSION}.ri sof-icl.ri
+
+cd ..
+ln -s sof-tplg-${SOF_VERSION} sof-tplg
+
+echo "Done installing Intel firmware and topology $SOF_VERSION"
